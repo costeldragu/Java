679dfcfde60addce93d252d573c721ad0d211b86
Dragu Costel <dragu.costel@yahoo.com>
2017-01-07 18:40:48 +0200

    Collections

---
+++ src/com/mdc/Collections/Lists.java
@@ -0,0 +1,7 @@
+package com.mdc.Collections;
+
+/**
+ * Created by CaDyMaN on 07.01.2017.
+ */
+public class Lists {
+}
--- src/com/mdc/TeachYourselfJava/Stream/Main.java
+++ src/com/mdc/TeachYourselfJava/Stream/Main.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
  * Created by CaDyMaN on 05.01.2017.
  */
 public class Main {
-    FileInputStream file  = new FileInputStream("D:\\data.txt");
+    //FileInputStream file  = new FileInputStream("D:\\data.txt");
 
 
 }
