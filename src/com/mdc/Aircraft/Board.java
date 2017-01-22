cde526320198108aef4ae84a82f75a91d20d4848
Dragu Costel <dragu.costel@yahoo.com>
2017-01-22 17:19:18 +0200

    Multiple implementations

--- src/com/mdc/Aircraft/Board.java
+++ src/com/mdc/Aircraft/Board.java
@@ -1,6 +1,7 @@
 package com.mdc.Aircraft;
 
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
@@ -14,11 +15,11 @@ public class Board extends Group {
         Pane topLeft = new Pane();
         Pane bottomRight = new Pane();
         GridPane gp = new GridPane();
+        Scene s = new Scene(this,300,300);
 
         topLeft.getStyleClass().add("board-top-left");
         bottomRight.getStyleClass().add("board-bottom-left");
         gp.getStyleClass().add("board-top-left");
-
 
 
         getChildren().add(topLeft);
