8056e1d873fe06a1f10548ae628425051afaa581
Dragu Costel <dragu.costel@yahoo.com>
2017-01-26 22:36:43 +0200

    FX8 - Drag&Drop

--- src/com/mdc/Fx/Main.java
+++ src/com/mdc/Fx/Main.java
@@ -3,6 +3,9 @@ package com.mdc.Fx;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.scene.layout.VBox;
@@ -24,12 +27,16 @@ public class Main extends Application {
         Button exitBtn = new Button("Exit");
 
         root.getChildren().add(msg);
-        Scene scene = new Scene(root, 300, 50);
+        Scene scene = new Scene(root, 300, 350);
         stage.setScene(scene);
         stage.setTitle("Hello JavaFX Application with a Scene");
         stage.show();
 
         exitBtn.setOnAction(event -> Platform.exit());
         root.getChildren().add(exitBtn);
+        Rectangle r = new Rectangle(75, 50, 50, 20);
+        r.setEffect(new DropShadow());
+        r.getTransforms().add(new Translate(100,100));
+        root.getChildren().add(r);
     }
 }
