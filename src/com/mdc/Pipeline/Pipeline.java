e4dd21ed88747693b3794202cf2b3052574d09de
Dragu Costel <costel.dragu@endava.com>
2017-07-29 12:52:28 +0300

    Pipe Line

---
+++ src/com/mdc/Pipeline/Pipeline.java
@@ -0,0 +1,19 @@
+package com.mdc.Pipeline;
+
+import java.util.*;
+
+public class Pipeline {
+    public static void main(String[] args) {
+        // write your code here
+        List<String> list = Arrays.asList("Toby", "Anna", "Leroy", "Alex");
+        List<String> filtered = new ArrayList<>();
+        for (String name: list) {
+            if (name.length() == 4) filtered.add(name);
+        }
+        Collections.sort(filtered);
+        Iterator<String> iterator = filtered.iterator();
+        if (iterator.hasNext()) System.out.println(iterator.next());
+        if (iterator.hasNext()) System.out.println(iterator.next());
+
+    }
+}
