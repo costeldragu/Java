4a3558db2979c122d6a64d0ac1eae1c4eba68749
DRAGU Costel <costel.dragu@endava.com>
2017-08-08 14:48:16 +0300

    Narcissistic number

---
+++ src/com/mdc/enva/NarcissisticNumber/Main.java
@@ -0,0 +1,21 @@
+package com.mdc.enva.NarcissisticNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println(" Please enter the input number :");
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        if (s.length() > 0) {
+            Integer length = s.length();
+            for (int x = 0; x < s.length(); ++x) {
+
+            }
+        }
+
+
+    }
+}
