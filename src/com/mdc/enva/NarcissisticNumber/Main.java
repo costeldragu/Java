fec70f098675be7cc71aa062d11eab13e271b29f
DRAGU Costel <costel.dragu@endava.com>
2017-08-08 16:08:09 +0300

    Narcissistic number

--- src/com/mdc/enva/NarcissisticNumber/Main.java
+++ src/com/mdc/enva/NarcissisticNumber/Main.java
@@ -11,8 +11,19 @@ public class Main {
         String s = in.nextLine();
         if (s.length() > 0) {
             Integer length = s.length();
+            Double results = 0.0;
             for (int x = 0; x < s.length(); ++x) {
+                Character n = s.charAt(x);
+                System.out.println("x:" + x + " | results: " + results + " | length: " + length + " n:" + Integer.valueOf(n.toString()));
+                results += Math.pow(Double.valueOf(n.toString()), (double) length.intValue());
 
+            }
+            if (results.equals(Double.valueOf(s))) {
+                System.out.println("The number is Narcissistic");
+                System.out.println("Results:" + results);
+            } else {
+                System.out.println("The number is not Narcissistic");
+                System.out.println("Results:" + results);
             }
         }
 
