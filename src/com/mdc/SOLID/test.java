7bd371b64208880e701aa9e3532547abb8377b63
DRAGU Costel <costel.dragu@endava.com>
2017-09-21 17:27:28 +0300

    Testing

--- src/com/mdc/SOLID/test.java
+++ src/com/mdc/SOLID/test.java
@@ -1,4 +1,27 @@
 package com.mdc.SOLID;
 
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
 public class test {
+    @Test
+    public void StringTest() {
+        String line = new String("-");
+        String secondLine = line.concat("-");
+        String secondLine1 = line.concat("- -");
+        System.out.println(line == secondLine);
+        System.out.println(" ");
+        System.out.println(line.length());
+        System.out.println(secondLine1.length());
+        System.out.println(secondLine);
+        System.out.println(secondLine1);
+    }
+
+    @Test
+    public void predicateTest() {
+//        StringBuilder c = "-ef";
+//        Predicate dash = c -> c.startsWith("‐");
+//        System.out.println(dash.test(""));
+    }
 }
