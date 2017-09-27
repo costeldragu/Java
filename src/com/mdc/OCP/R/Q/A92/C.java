e657b44049dcc998f5ba8357d449f2e7dc031d05
DRAGU Costel <costel.dragu@endava.com>
2017-09-27 16:56:04 +0300

    Questions

---
+++ src/com/mdc/OCP/R/Q/A92/C.java
@@ -0,0 +1,12 @@
+package com.mdc.OCP.R.Q.A92;
+
+public class C extends B{
+
+    public C() {
+        System.out.println("C");
+    }
+
+    public static void main(String... a) {
+        C c = new C();
+    }
+}
